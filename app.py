452c10b74885906801ea2eb3b93ed0c033ec4f99
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2025-02-22 20:31:37 +0530

    Modifying app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-import streamlit as st
+
 import google.generativeai as genai
+import streamlit as st
 from dotenv import load_dotenv
 from github import Github
 
@@ -15,9 +16,9 @@ ghub = Github(os.getenv("GITHUB_TOKEN"))
 
 # ------------------Functions--------------------
 
-def get_genai_response(prompt):
+def get_genai_response(prompt, text):
     model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(prompt)
+    response = model.generate_content([prompt, text])
     return response.text
 
 
@@ -33,13 +34,13 @@ def get_content_from_github(link):
                 contents.extend(repo.get_contents(file_content.path))
             else:
                 try:
-                    print(file_content.decoded_content.decode('utf-8'))
+                    # print(file_content.decoded_content.decode('utf-8'))
                     final_content += " " + file_content.decoded_content.decode('utf-8')
                 except Exception as e:
                     print(f"Could not read {file_content.path}: {e}")
 
         return final_content
-    return ""
+    return None
 
 
 def verify_link(link):
@@ -48,10 +49,24 @@ def verify_link(link):
     return False
 
 
+# ---------------------Prompt-------------------
+input_prompt = """
+You are Github repo content expert with great knowledge about the code. 
+Generate a good and interesting README with proper functionality and working.
+Also try to add subheader project link and add project link which may be present in README.md.
+The extracted content from github repo is :
+"""
+
 # --------------------UI-------------------------
 
 
-st.set_page_config(page_title="README  Generator")
-st.header("Readme Generator")
+st.set_page_config(page_title="README Generator")
+st.header("README Generator")
 github_link = st.text_input("Enter the github link")
-submit_button = "Extract"
+submit_button = st.button("Extract")
+if submit_button:
+    with st.spinner("Loading"):
+        content = get_content_from_github(github_link)
+        if content is not None:
+            result = get_genai_response(input_prompt, content)
+            st.write(result)
